0c3df29858fad29c5064d20d3df6e3e4363f738c
Elder Bol <elder.bol@uneatlantico.es>
2023-06-14 00:39:32 +0200

    docs(exam): commit

---
+++ entregas/paulaDiestro/examen-final-p2/src/com/progra/hangman/UserInterface.java
@@ -0,0 +1,104 @@
+package com.progra.hangman;
+
+import com.progra.hangman.base.Word;
+
+import java.util.*;
+
+public class UserInterface {
+    List<Word> words;
+    int currentWordIndex;
+    boolean gameOver;
+
+    HangmanLogic logic;
+
+    public UserInterface() {
+        this.words = new ArrayList<>();
+        this.currentWordIndex = 0;
+    }
+
+    private int randomIndex() {
+        Random random = new Random();
+        return random.nextInt(this.words.size());
+    }
+
+    public int sizeListWord() {
+        return words.size();
+    }
+
+    private void loadData(String filename){
+
+
+        /*
+        Programa aquí la funcionalidad para cargar las palabras desde el archivo filename
+        i: leer el archivo filename y guardar las palabras en la lista words
+        i: si el archivo no existe, lanzar una excepción FileNotFoundException
+        i: si el archivo no contiene palabras, lanzar una excepción InvalidWordException
+
+        Usar el método WordParser.parse para parsear las palabras
+
+        */
+
+    }
+
+    public void start(String filename) {
+        /*
+         Cargar las palabras desde el archivo
+
+         EJEMPLO DE UNA LINEA DEL ARCHIVO:
+             1,Electroencefalografista,LARGA
+
+             Con la siguiente estructura:
+             codigo,palabra,tipo
+
+         Inicializar el juego con una palabra aleatoria
+
+         * */
+    }
+
+    public void play() {
+        Scanner reader = new Scanner(System.in);
+
+
+        System.out.println("==========================================================");
+        System.out.println("=                      AHORCADO                          =");
+        System.out.println("==========================================================");
+        System.out.println("");
+        printMenu();
+
+        while (!this.gameOver && !this.logic.isWon()) {
+
+
+            System.out.println("\nPalabra a adivinar con " + this.logic.hiddenWord().length() + " letras:");
+            System.out.println( this.logic.hiddenWord());
+
+            System.out.println("Ingrese una letra: ");
+            String letter = reader.nextLine();
+
+            if (letter.equals("salir")) {
+                System.out.println("Gracias por jugar!");
+                break;
+            }else if (letter.length()==1) {
+                this.logic.guessLetter(letter.toUpperCase());
+            } else if (letter.isEmpty()) {
+                printMenu();
+            }
+
+            System.out.println(this.logic.getHangman());
+            this.gameOver = this.logic.isGameOver();
+
+        }
+        if (this.logic.isWon()) {
+            System.out.println("Ganaste!");
+        } else {
+            System.out.println("Perdiste!");
+        }
+    }
+
+
+    public void printMenu() {
+        System.out.println("* Instrucciones *");
+        System.out.println("salir   -  Salir del juego");
+        System.out.println("Para jugar, ingrese una letra y presione enter. ");
+    }
+
+}
---
+++ entregas/paulaDiestro/examen-final-p2/src/com/progra/hangman/base/LargeWord.java
@@ -0,0 +1,8 @@
+package com.progra.hangman.base;
+
+public class LargeWord extends Word{
+    public LargeWord(int id, String word) {
+        super(id, word);
+    }
+
+}
