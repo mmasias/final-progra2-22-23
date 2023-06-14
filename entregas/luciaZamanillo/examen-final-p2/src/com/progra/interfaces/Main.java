96e35e4ecc3c1ff21c6b27b845170037c5d46ac4
luciaZama <lzamanillol@gmail.com>
2023-06-14 16:13:42 +0200

    feature: added exercise two

--- entregas/luciaZamanillo/examen-final-p2/src/com/progra/interfaces/Main.java
+++ entregas/luciaZamanillo/examen-final-p2/src/com/progra/interfaces/Main.java
@@ -1,7 +1,9 @@
 package com.progra.interfaces;
 
+import com.progra.interfaces.exceptions.InputMaxValueException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMaxValueException {
         /*
          * Acá debe de probar la implementación del ejercicio dos, tal
          * como se detalla en el enunciado.
@@ -9,6 +11,14 @@ public class Main {
         System.out.println("==================================");
         System.out.println("===   Ejercicio 2: Interfaces  ===");
         System.out.println("==================================");
+
+        Calc calc = new Calc(6);
+        int divSum = calc.divisorSum(6);
+        String desDiv = calc.describeDivisors(6);
+
+        System.out.println("divisorSum result: " + divSum);
+        System.out.println("describeDivisor result: " + desDiv);
+
 
 
     }
