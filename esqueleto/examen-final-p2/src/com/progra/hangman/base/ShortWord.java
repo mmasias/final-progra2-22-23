bd1d177dd5119d213aad28f0ed96db55cd7c73ae
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2023-06-14 15:29:55 +0200

    Delete ShortWord.java

--- esqueleto/examen-final-p2/src/com/progra/hangman/base/ShortWord.java
+++
@@ -1,9 +0,0 @@
-package Hangman.Bases;
-
-public class ShortWord extends Word {
-    public ShortWord(int id, String word) {
-        super(id, word);
-        this.level = "S";
-        this.maxTries=6;
-    }
-}
