dba16a520763ec68c87cf277ef2b30ab8d140d31
Daniel <daniel.roldan@alumnos.uneatlantico.es>
2023-06-14 16:13:03 +0200

    Main Interfaces finalizado

--- entregas/roldanDaniel/esqueleto/examen-final-p2/src/com/progra/interfaces/Main.java
+++ entregas/roldanDaniel/esqueleto/examen-final-p2/src/com/progra/interfaces/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("==================================");
 
         Calc calc = new Calc();
-        System.out.println("Suma de los divisores: " + calc.divisorSum(0));
+        System.out.println("Suma de los divisores: " + calc.divisorSum(1002));
 
     }
 }
