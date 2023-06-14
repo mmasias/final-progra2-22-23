e871267add4efa483e821c2895b0ecdb92af1212
ana-deb <74409467+ana-deb@users.noreply.github.com>
2023-06-14 16:23:40 +0200

    Delete entrega/AnaDíazDeEntresotosBalbontín/ExamenFinalPrograII directory

--- entrega/AnaDíazDeEntresotosBalbontín/ExamenFinalPrograII/examen-final-p2/src/com/progra/hangman/parsers/Parser.java
+++
@@ -1,9 +0,0 @@
-package com.progra.hangman.parsers;
-
-import com.progra.Exception.InvalidWordException;
-import com.progra.hangman.base.Word;
-
-    public interface Parser {
-        public Word parse(String tokens) throws InvalidWordException;
-    }
-
