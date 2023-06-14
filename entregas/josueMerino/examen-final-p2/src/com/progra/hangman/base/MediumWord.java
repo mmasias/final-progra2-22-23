6c4206c1950ce2fa4346330f255eaa103902d949
josueMerino <josue.merino@alumnos.uneatlantico.es>
2023-06-14 15:22:08 +0200

    feature: Excersice 1 started with WordParser

---
+++ entregas/josueMerino/examen-final-p2/src/com/progra/hangman/base/MediumWord.java
@@ -0,0 +1,7 @@
+package com.progra.hangman.base;
+
+public class MediumWord extends Word {
+    public MediumWord(int code, String word) {
+        super(code, word);
+    }
+}
---
+++ entregas/josueMerino/examen-final-p2/src/com/progra/hangman/base/ShortWord.java
@@ -0,0 +1,7 @@
+package com.progra.hangman.base;
+
+public class ShortWord extends Word {
+    public ShortWord(int code, String word) {
+        super(code, word);
+    }
+}
