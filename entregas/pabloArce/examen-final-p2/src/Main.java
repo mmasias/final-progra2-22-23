9c52e6088f590e809ab3f38107ad182faab64046
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2023-06-14 14:44:44 +0200

    Copy of initial structure

--- entregas/pabloArce/examen-final-p2/src/Main.java
+++ entregas/pabloArce/examen-final-p2/src/Main.java
@@ -1,5 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+         * Este código lo he puesto en el Main para que puedas probar
+         * el juego de ahorcado.
+         * El ejemplo de la salida por consola lo puedes ver en el
+         * archivo docs/ejemplo-salida-consola.txt
+         */
+
+        UserInterface ui = new UserInterface();
+        ui.start("data/word-list.txt");
+        ui.play();
     }
 }
