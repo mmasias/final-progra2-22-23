eeff7c6db7046e8bf5fbd1cb28f7b071a44688f9
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2023-06-14 15:26:36 +0200

    Add files via upload

---
+++ entregas/raulPelayo/com.progra.interfaces/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Calc myCalculator = new Calc();
+        System.out.println("I implemented: ");
+        System.out.println(myCalculator.divisorSum(1000));
+        System.out.println(myCalculator.describeDivisors(1000));
+
+    }
+
+}
