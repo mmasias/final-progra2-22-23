907deca4bb98982ce50a62fabc4d14656c439602
Jorgeog25 <jrotero258@gmail.com>
2023-06-14 15:02:19 +0200

    fea(final): upload the final exam

---
+++ entregas/jorgeOtero/examen-final-p2/src/com/progra/hangman/base/ShortWord.java
@@ -0,0 +1,10 @@
+package com.progra.hangman.base;
+
+public class ShortWord extends Word{
+    public ShortWord(int id, String word) {
+        super(id, word);
+        this.level = "Short";
+        this.maxTries = 1;
+    }
+}
+
---
+++ entregas/jorgeOtero/examen-final-p2/src/com/progra/hangman/parsers/WordParser.java
@@ -0,0 +1,76 @@
+package com.progra.hangman.parsers;
+
+import com.progra.hangman.base.LargeWord;
+import com.progra.hangman.base.MediumWord;
+import com.progra.hangman.base.ShortWord;
+import com.progra.hangman.base.Word;
+import com.progra.hangman.exceptions.InvalidIdException;
+import com.progra.hangman.exceptions.InvalidWordException;
+
+public class WordParser implements Parser {
+
+    String regex; // Cadena de texto que representa el patrón de expresión regular
+    final int ELEMENT_COUNT = 3; // Número de elementos que debe contener la cadena de texto al hacer split
+
+    public WordParser() {
+        // Regex por defecto, separa por comas "," la cadena de texto que se le pasa al método parse
+        this.regex = ",";
+    }
+
+    public WordParser(String regex) {
+        this.regex = regex;
+    }
+
+    public Word parse(String tokens) throws InvalidWordException {
+
+        String[] words = tokens.split(this.regex);
+        if(words[2] == "CORTA") {
+            try {
+                return new ShortWord(this.idValidator(words[0]), words[1]);
+            } catch (InvalidIdException e) {
+                System.err.println(e.getMessage());
+            }
+        } else if (words[2]== "MEDIA"){
+            try {
+                return new MediumWord(this.idValidator(words[0]), words[1]);
+            } catch (InvalidIdException e) {
+                System.err.println(e.getMessage());
+            }
+        } else if (words[2]== "LARGA"){
+            try {
+                return new LargeWord(this.idValidator(words[0]), words[1]);
+            } catch (InvalidIdException e) {
+                System.err.println(e.getMessage());
+            }
+        } else {
+            throw new InvalidWordException("Tipo de palabra no válida");
+        }
+        return null;
+    }
+
+    /*
+     Parsea el Id de la palabra si ocurre un error lanza una excepción InvalidIdException
+    */
+    private int idValidator(String id) throws InvalidIdException {
+        int idInt;
+
+        try{
+            idInt = Integer.parseInt(id);
+        }catch(Exception e){
+            throw new InvalidIdException(e.getMessage());
+        }
+
+        return idInt;
+    }
+
+    /*
+    * Valida que la cadena contenga 3 elementos separados por el caracter regex caso contrario lanza una excepción InvalidWordException
+     */
+    private void sizeValidator(String[] words) throws InvalidWordException {
+        if(words.length != this.ELEMENT_COUNT){
+            throw new InvalidWordException("Valores de la palabra faltantes");
+        }
+    }
+
+
+}
